be017c650748d9914c5a15ef8d0c9d647f35007d
balza <paolo.balzarotti@gmail.com>
2013-08-27 16:58:01 +0200

    enhanced forms behaviour

--- timesheet/forms.py
+++ timesheet/forms.py
@@ -1,15 +1,15 @@
 from django import forms
-from django.forms import Form,ModelForm,ChoiceField,TextInput
+from django.forms import Form,ModelForm,ChoiceField,TextInput,ModelChoiceField
 from leantracker.projects.models import Project
 from leantracker.timesheet.models import Timesheet
 
 class TimesheetForm(Form):
-   projects = forms.ChoiceField(choices=[("*", "---")] + [(x.id, x.name) for x in Project.group_projects.all()])
-   mon = forms.IntegerField(max_value=8,min_value=0,widget=forms.TextInput(attrs={'size':'2'}))
-   tue = forms.IntegerField(max_value=8,min_value=0,widget=forms.TextInput(attrs={'size':'2'}))
-   wed = forms.IntegerField(max_value=8,min_value=0,widget=forms.TextInput(attrs={'size':'2'}))
-   thu = forms.IntegerField(max_value=8,min_value=0,widget=forms.TextInput(attrs={'size':'2'}))
-   fri = forms.IntegerField(max_value=8,min_value=0,widget=forms.TextInput(attrs={'size':'2'}))
-   sat = forms.IntegerField(max_value=8,min_value=0,widget=forms.TextInput(attrs={'size':'2'}))
-   sun = forms.IntegerField(max_value=8,min_value=0,widget=forms.TextInput(attrs={'size':'2'}))
+    projects = forms.ModelChoiceField(queryset=Project.objects.all(), empty_label="-------")
+    mon = forms.IntegerField(max_value=8,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}))
+    tue = forms.IntegerField(max_value=8,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}))
+    wed = forms.IntegerField(max_value=8,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}))
+    thu = forms.IntegerField(max_value=8,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}))
+    fri = forms.IntegerField(max_value=8,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}))
+    sat = forms.IntegerField(max_value=8,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}))
+    sun = forms.IntegerField(max_value=8,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}))
 
--- timesheet/views.py
+++ timesheet/views.py
@@ -1,5 +1,6 @@
 from leantracker.timesheet.forms import TimesheetForm
 from django.forms.formsets import formset_factory
+from django.template.context import RequestContext
 from django.shortcuts import render_to_response
 
 def manage_timesheet(request):
@@ -10,5 +11,6 @@ def manage_timesheet(request):
            return render_to_response('timesheet/index.html')
     else:
         formset = TimesheetFormSet()
-    return render_to_response('timesheet/timesheet_form.html', {'formset': formset})
+    return render_to_response('timesheet/timesheet_form.html', {'formset': formset}, context_instance=RequestContext(request))
+
 
