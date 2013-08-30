d9b085f54c4dab7ceb9f18674f82fcfe8c812b1e
Paolo <balza@hotmail.com>
2013-08-30 18:22:55 +0200

    Form validation

--- timesheet/forms.py
+++ timesheet/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.forms import Form,ModelForm,ChoiceField,TextInput,ModelChoiceField
+from django.forms.formsets import BaseFormSet
 from leantracker.projects.models import Project
 from leantracker.timesheet.models import Timesheet
 from datetime import date, timedelta
 from django.utils.safestring import mark_safe
+
 
 #TODO: spostare in una classe adeguata
 def first_day_of_this_week(today):
@@ -22,18 +24,33 @@ def week_start_date(year, week):
     return d + delta
 
 class TimesheetForm(Form):
-    #TODO: filtrare solo per il gruppo dell'utente
-    project = forms.ModelChoiceField(queryset=Project.objects.all(), empty_label="-------")
+	#TODO: filtrare solo per il gruppo dell'utente
+    project = forms.ModelChoiceField(required=False,queryset=Project.objects.all(), empty_label="-------")
     delta = timedelta(days=1)
     monday = first_day_of_this_week(date.today())  
-    mon = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe(monday.strftime("%a <br/> %d %b")))
-    tue = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + delta).strftime("%a <br/> %d %b")))
-    wed = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 2 * delta).strftime("%a <br/> %d %b")))
-    thu = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 3 * delta).strftime("%a <br/> %d %b")))
-    fri = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 4 * delta).strftime("%a <br/> %d %b")))
-    sat = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 5 * delta).strftime("%a <br/> %d %b")))
-    sun = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 6 * delta).strftime("%a <br/> %d %b")))
+    mon = forms.IntegerField(required=False,max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe(monday.strftime("%a <br/> %d %b")))
+    tue = forms.IntegerField(required=False,max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + delta).strftime("%a <br/> %d %b")))
+    wed = forms.IntegerField(required=False,max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 2 * delta).strftime("%a <br/> %d %b")))
+    thu = forms.IntegerField(required=False,max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 3 * delta).strftime("%a <br/> %d %b")))
+    fri = forms.IntegerField(required=False,max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 4 * delta).strftime("%a <br/> %d %b")))
+    sat = forms.IntegerField(required=False,max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 5 * delta).strftime("%a <br/> %d %b")))
+    sun = forms.IntegerField(required=False,max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 6 * delta).strftime("%a <br/> %d %b")))
 
-    
-    
+    def __init__(self, *args, **kwargs):
+        super(TimesheetForm, self).__init__(*args, **kwargs)        
+
+    def clean(self):  
+        print "TimesheetForm clean"  
+        cleaned_data = super(TimesheetForm, self).clean()        
+        mon = cleaned_data.get("mon")
+        tue = cleaned_data.get("tue")        
+        if mon==0 and tue==0 :
+            raise forms.ValidationError("Did not send for 'help' in "
+                            "the subject despite CC'ing yourself.")
+        return cleaned_data
+
+class BaseTimesheetFormSet(BaseFormSet): 
+    def clean(self):
+        if any(self.errors):            
+            return 
 
--- timesheet/views.py
+++ timesheet/views.py
@@ -1,19 +1,21 @@
-from leantracker.timesheet.forms import TimesheetForm
+from leantracker.timesheet.forms import TimesheetForm,BaseTimesheetFormSet
 from django.forms.formsets import formset_factory
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response
 from django.forms.models import inlineformset_factory
 
 def create_timesheet(request):
-    TimesheetFormSet = formset_factory(TimesheetForm)
+    TimesheetFormSet = formset_factory(TimesheetForm,formset=BaseTimesheetFormSet)
     if request.method == 'POST':
-		if 'add_row' in request.POST:
-			cp = request.POST.copy()
-			cp['form-TOTAL_FORMS'] = int(cp['form-TOTAL_FORMS']) + 1
-			formset = TimesheetFormSet(cp, request.FILES)			
-		elif 'submit' in request.POST:
-			if formset.is_valid():
-				return render_to_response('timesheet/index.html')			
+        if 'add_row' in request.POST:
+            cp = request.POST.copy()
+            cp['form-TOTAL_FORMS'] = int(cp['form-TOTAL_FORMS']) + 1
+            formset = TimesheetFormSet(cp, request.FILES)           		
+        elif 'submit' in request.POST:
+            print "create_timesheet submit"
+            formset = TimesheetFormSet(request.POST, request.FILES)        
+            if formset.is_valid():
+                return render_to_response('timesheet/index.html')			
     else:
         formset = TimesheetFormSet()
     return render_to_response('timesheet/timesheet_form.html', {'formset': formset}, context_instance=RequestContext(request))
