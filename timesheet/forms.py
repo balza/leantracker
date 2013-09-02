c9b7ed2bc65c98588beb7c76fdef4dd8cc4c4a48
balza <paolo.balzarotti@gmail.com>
2013-09-02 17:44:26 +0200

    Add new rows

--- timesheet/forms.py
+++ timesheet/forms.py
@@ -26,13 +26,13 @@ class TimesheetForm(Form):
     project = forms.ModelChoiceField(queryset=Project.objects.all(), empty_label="-------")
     delta = timedelta(days=1)
     monday = first_day_of_this_week(date.today())  
-    mon = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe(monday.strftime("%a <br/> %d %b")))
-    tue = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + delta).strftime("%a <br/> %d %b")))
-    wed = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 2 * delta).strftime("%a <br/> %d %b")))
-    thu = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 3 * delta).strftime("%a <br/> %d %b")))
-    fri = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 4 * delta).strftime("%a <br/> %d %b")))
-    sat = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 5 * delta).strftime("%a <br/> %d %b")))
-    sun = forms.IntegerField(max_value=24,min_value=0,initial=0,widget=forms.TextInput(attrs={'size':'2'}),label=mark_safe((monday + 6 * delta).strftime("%a <br/> %d %b")))
+    mon = forms.IntegerField(max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe(monday.strftime("%a <br/> %d %b")))
+    tue = forms.IntegerField(max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + delta).strftime("%a <br/> %d %b")))
+    wed = forms.IntegerField(max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 2 * delta).strftime("%a <br/> %d %b")))
+    thu = forms.IntegerField(max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 3 * delta).strftime("%a <br/> %d %b")))
+    fri = forms.IntegerField(max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 4 * delta).strftime("%a <br/> %d %b")))
+    sat = forms.IntegerField(max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 5 * delta).strftime("%a <br/> %d %b")))
+    sun = forms.IntegerField(max_value=24,min_value=0,widget=forms.TextInput(attrs={'size':'2','value':'0'}),label=mark_safe((monday + 6 * delta).strftime("%a <br/> %d %b")))
 
     
     
--- timesheet/views.py
+++ timesheet/views.py
@@ -5,15 +5,23 @@ from django.shortcuts import render_to_response
 from django.forms.models import inlineformset_factory
 
 def create_timesheet(request):
-    TimesheetFormSet = formset_factory(TimesheetForm)
+    TimesheetFormSet = formset_factory(TimesheetForm, can_delete=True)
     if request.method == 'POST':
-		if 'add_row' in request.POST:
-			cp = request.POST.copy()
-			cp['form-TOTAL_FORMS'] = int(cp['form-TOTAL_FORMS']) + 1
-			formset = TimesheetFormSet(cp, request.FILES)			
-		elif 'submit' in request.POST:
-			if formset.is_valid():
-				return render_to_response('timesheet/index.html')			
+        if 'add_row' in request.POST:
+            formset = TimesheetFormSet(request.POST, request.FILES)
+            if formset.is_valid():
+                cp = request.POST.copy()
+                cp['form-TOTAL_FORMS'] = int(cp['form-TOTAL_FORMS']) + 1 
+                formset = TimesheetFormSet(cp,request.FILES)           
+        elif 'submit' in request.POST:
+            formset = TimesheetFormSet(request.POST, request.FILES)
+            if formset.is_valid():
+                return render_to_response('timesheet/index.html')
+        elif 'delete_rows' in request.POST:      
+            formset = TimesheetFormSet(request.POST, request.FILES)
+            if formset.is_valid():
+                for form in formset.deleted_forms:
+                    form.cleaned_data                    
     else:
         formset = TimesheetFormSet()
     return render_to_response('timesheet/timesheet_form.html', {'formset': formset}, context_instance=RequestContext(request))
