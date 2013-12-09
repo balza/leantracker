c99264efe5726d69840de705149b572c62bed1f4
balza <paolo.balzarotti@gmail.com>
2013-12-09 18:43:05 +0100

    Added REST library for django

--- timesheet/serializers.py
+++ timesheet/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from timesheet.models import TimeEntry
 
 
-class TimeEntrySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = TimeEntry
-        fields = ('project', 'hours', 'user','reg_date','timesheet')
+class TimeEntrySerializer(serializers.Serializer):
+    hours = serializers.IntegerField()
+    reg_date = serializers.DateField()
+
+    def restore_object(self, attrs, instance=None):
+        if instance:
+            # Update existing instance
+            instance.hours = attrs.get('hours', instance.week_number)
+            instance.reg_date = attrs.get('reg_date', instance.reg_date)
+            return instance
+        return TimeEntry(**attrs)
