b8cc606e931334cb3844d5ed9e083f54d993aa9b
balza <paolo.balzarotti@gmail.com>
2013-12-11 17:37:11 +0100

    Added REST library for django

--- timesheet/serializers.py
+++ timesheet/serializers.py
@@ -7,19 +7,16 @@ from django.contrib.auth.models import User
 class TimeEntrySerializer(serializers.Serializer):
     project_id = serializers.IntegerField()
     hours = serializers.IntegerField()
-    username = serializers.CharField()
+    user_id = serializers.IntegerField()
     reg_date = serializers.DateField()
     timesheet_id = serializers.IntegerField()
 
     def restore_object(self, attrs, instance=None):
         if instance:
-            project_id = attrs.get('project_id')
-            instance.project = Project.objects.get(project_id)
+            instance.project = attrs.get('project_id', instance.project_id)
             instance.hours = attrs.get('hours', instance.week_number)
             instance.reg_date = attrs.get('reg_date', instance.reg_date)
-            username = attrs.get('username')
-            instance.user = User.objects.get(username)
-            timesheet_id = attrs.get('timesheet_id')
-            instance.timesheet = Timesheet.objects.get(timesheet_id)
+            instance.user_id = attrs.get('user_id', instance.user_id)
+            instance.timesheet_id = attrs.get('timesheet_id', instance.timesheet_id)
             return instance
         return TimeEntry(**attrs)
